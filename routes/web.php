b9a36f8d342e05eb87cb413db0b27f72d425a32f
Frank Fava <frank.fava@gmail.com>
2024-09-28 23:37:07 +0200

    Keep Inertia but use default blade views

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,9 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
-use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    return view('welcome');
 });
